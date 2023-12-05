29c46869e9408f62718dff343c8d72bf362744b4
Raihansyah-for-Capstone <rai.machine.learning.explore@gmail.com>
2023-12-05 21:54:50 +0700

    Adding bridge Token

--- API 1b/backend/js_sessionHandler.js
+++ API 1b/backend/js_sessionHandler.js
@@ -7,6 +7,19 @@ const signUART = (userPayload) => {
   return signer.signThis(userPayload, HMACSHAKEY)
 }
 
+const requestBridge = async (UART) => {
+  const func = 'requestBridge'
+  try {
+    const validatedUART = signer.apply(UART, HMACSHAKEY)
+    const username = validatedUART.username
+    const bridgeToken = signer.signThis({ username }, HMACSHAKEY, 10)
+    return bridgeToken
+  } catch (e) {
+    if (e instanceof SessionError) throw e
+    else throw new SessionError(func, 'unknown')
+  }
+}
+
 const validateUART = async (UART) => {
   const func = 'validateUART'
   try {
@@ -14,14 +27,17 @@ const validateUART = async (UART) => {
     const hUARTUsername = signer.simpleHash(validatedUART.username)
     const UARTActiveSession = validatedUART.activeSession
     const userActiveSession = await fire('userdata', hUARTUsername).get('activeSession')
+    const hUserActiveSession = signer.simpleHash(userActiveSession, 'default')
 
-    if (UARTActiveSession === userActiveSession) {
+    signer.compareHash(UARTActiveSession, hUserActiveSession)
 
-    } else {
-      throw new SessionError(func, 'activeSessionMismatch')
-    }
+    return true
   } catch (e) {
     if (e instanceof SignerError) throw e
     else throw new SessionError(func, 'unknown')
   }
+}
+module.exports = {
+  validateUART,
+  requestBridge
 }
